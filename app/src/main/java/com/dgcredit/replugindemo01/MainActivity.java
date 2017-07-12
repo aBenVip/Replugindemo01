b895203daa83bb596d58c5a0f5c189630dec7423
aben <abenvip@163.com>
2017-07-12 17:38:09 +0800

    commit

--- app/src/main/java/com/dgcredit/replugindemo01/MainActivity.java
+++ app/src/main/java/com/dgcredit/replugindemo01/MainActivity.java
@@ -1,16 +1,15 @@
 package com.dgcredit.replugindemo01;
 
-import android.content.res.AssetManager;
 import android.content.res.Resources;
-import android.os.Binder;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.qihoo360.replugin.RePlugin;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Date;
@@ -25,14 +24,20 @@ public class MainActivity extends AppCompatActivity {
         ImageView ivShow = (ImageView) findViewById(R.id.imageView);
 
         ClassLoader classLoader = RePlugin.getHostClassLoader();
-        //加载宿主中图片资源
 
-
-        Resources resources = RePlugin.getHostContext().getResources();
-        int identifier = resources.getIdentifier("icon_app", "mipmap", "com.dgcredit.repluginhostdemo");
+        //加载宿主中图片资源，测试不能用
+       /* Resources resources = RePlugin.getHostContext().getResources();
+        int identifier = resources.getIdentifier("icon_app", "drawable", "com.dgcredit.repluginhostdemo");
         Log.i("TAG",identifier+"++++++++++++++++");
-        ivShow.setImageResource(identifier);
-
+        ivShow.setImageResource(identifier);*/
+        try{
+            Class clazz = classLoader.loadClass("com.dgcredit.repluginhostdemo.R$drawable");
+            Field field = clazz.getField("bg_about");
+            int identifier = (int)field.get(null);
+            ivShow.setImageResource(identifier);
+        }catch(Exception e){
+            Log.i("Loader", "error:"+Log.getStackTraceString(e));
+        }
 
         //加载宿主中的工具类
         try {
